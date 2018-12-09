aad38f65ce539a0ff81f405bc4a99efd9fcbe843
Stan Livian <livian.stan@gmail.com>
2018-12-09 15:20:34 +0200

    TemaFinalBunRandomNumber

--- TemaRNG/Program.cs
+++ TemaRNG/Program.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System;
 
-namespace TemaRNG
+public class Program
 {
-    class Program
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Random rng = new Random();
+        int numberToGuess = rng.Next(1, 100);
+        int userGuess = 0;
+        while (userGuess != numberToGuess)
         {
+            Console.Write("Ghiceste numarul: ");
+            int.TryParse(Console.ReadLine(), out userGuess);
+            if (userGuess > numberToGuess)
+            {
+                Console.Write("Ghiceste numarul: ");
+                Console.WriteLine("{0} e prea mare!", userGuess);
+            }
+            else if (userGuess < numberToGuess)
+            {
+                Console.WriteLine("{0} e prea mic !", userGuess);
+            }
+            else
+            {
+                Console.WriteLine("{0} e corect! Felicitari", userGuess);
+                Console.ReadLine();
+            }
         }
     }
 }
